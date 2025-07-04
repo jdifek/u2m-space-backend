fa34a77072e2ec9ff2b4fe02a65df8bc5e8b9dcc
vladislavzaver91 <vladislavzaver91@gmail.com>
2025-07-04 19:20:05 +0300

    feat: updated search

--- src/routes/classifiedsRoutes.js
+++ src/routes/classifiedsRoutes.js
@@ -33,8 +33,8 @@ const {
 const { sendMessage } = require('../controllers/classifieds/sendMessage')
 const { proposeDeal } = require('../controllers/classifieds/proposeDeal')
 const {
-	searchClassifieds,
-} = require('../controllers/classifieds/searchClassifieds')
+	promoteClassified,
+} = require('../controllers/classifieds/promoteClassified')
 
 const router = express.Router()
 
@@ -47,11 +47,11 @@ const upload = multer({
 router.get('/api/classifieds', getAllClassifieds)
 router.get('/api/classifieds/user', authMiddleware, getUserClassifieds)
 router.get('/api/favorites/user', authMiddleware, getUserFavorites)
-router.get('/api/classifieds/:id', getClassifiedById)
-router.post('/api/classifieds/search', searchClassifieds)
 router.get('/api/classifieds/filter', filterClassifieds)
+router.get('/api/classifieds/:id', getClassifiedById)
 
 // Защищенные маршруты (требуют авторизации)
+router.post('/api/classifieds/:id/promote', authMiddleware, promoteClassified)
 router.post(
 	'/api/classifieds',
 	authMiddleware,
